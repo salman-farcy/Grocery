463aa02a10217ba9016896067a5502b5a9f705b1
slaman-farcy <salmanfarcy253@gmail.com>
2025-06-27 01:01:06 +0600

    some code add

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import Navbar from './components/Navbar'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
 
 const App = () => {
     return (
         <div className="">
             <Navbar />
+            <div className="">
+                <Routes>
+                    <Route path='/' element={<Home/>} />
+                </Routes>
+            </div>
         </div>
     )
 
