445e86d0bf5133cce531bdf4ab49f1f17bb717ba
slaman-farcy <salmanfarcy253@gmail.com>
2025-11-17 07:53:19 +0600

    feat(category) add static category data and integrate category section UI

--- Client/src/assets/assets.js
+++ Client/src/assets/assets.js
@@ -100,50 +100,102 @@ export const assets = {
   box_icon,
 };
 
+// export const categories = [
+//   {
+//     text: "Organic veggies",
+//     path: "Vegetables",
+//     image: organic_vegitable_image,
+//     bgColor: "#FEF6DA",
+//   },
+//   {
+//     text: "Fresh Fruits",
+//     path: "Fruits",
+//     image: fresh_fruits_image,
+//     bgColor: "#FEE0E0",
+//   },
+//   {
+//     text: "Cold Drinks",
+//     path: "Drinks",
+//     image: bottles_image,
+//     bgColor: "#F0F5DE",
+//   },
+//   {
+//     text: "Instant Food",
+//     path: "Instant",
+//     image: maggi_image,
+//     bgColor: "#E1F5EC",
+//   },
+//   {
+//     text: "Dairy Products",
+//     path: "Dairy",
+//     image: dairy_product_image,
+//     bgColor: "#FEE6CD",
+//   },
+//   {
+//     text: "Bakery & Breads",
+//     path: "Bakery",
+//     image: bakery_image,
+//     bgColor: "#E0F6FE",
+//   },
+//   {
+//     text: "Grains & Cereals",
+//     path: "Grains",
+//     image: grain_image,
+//     bgColor: "#F1E3F9",
+//   },
+// ];
 export const categories = [
   {
+    id: 1,
     text: "Organic veggies",
     path: "Vegetables",
     image: organic_vegitable_image,
     bgColor: "#FEF6DA",
   },
   {
+    id: 2,
     text: "Fresh Fruits",
     path: "Fruits",
     image: fresh_fruits_image,
     bgColor: "#FEE0E0",
   },
   {
+    id: 3,
     text: "Cold Drinks",
     path: "Drinks",
     image: bottles_image,
     bgColor: "#F0F5DE",
   },
   {
+    id: 4,
     text: "Instant Food",
     path: "Instant",
     image: maggi_image,
     bgColor: "#E1F5EC",
   },
   {
+    id: 5,
     text: "Dairy Products",
     path: "Dairy",
     image: dairy_product_image,
     bgColor: "#FEE6CD",
   },
   {
+    id: 6,
     text: "Bakery & Breads",
     path: "Bakery",
     image: bakery_image,
     bgColor: "#E0F6FE",
   },
   {
+    id: 7,
     text: "Grains & Cereals",
     path: "Grains",
     image: grain_image,
     bgColor: "#F1E3F9",
   },
 ];
+
 
 export const footerLinks = [
   {
